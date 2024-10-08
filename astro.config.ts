45f5bb23de835a84cee4fd2f80db1c886ddcf469
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-10-09 01:18:56 +0900

    fix: 🚑️ (`astro.config.ts`) `site`に実行環境に合った正しい値が設定されるように修正した。  (#35)
    正しいオリジンが設定されるようにした。
    - ローカルの開発環境の時は、`http://localhost:4321`
    - Cloudflare Pagesの環境の時は、デプロイ先URLのオリジン
    - フォールバック時は`https://33.shikosai.net`

--- astro.config.ts
+++ astro.config.ts
@@ -5,20 +5,34 @@ import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
 import { defineConfig } from 'astro/config';
 
-const isProduction = process.env.NODE_ENV === 'production';
+/**
+ * Astroの環境変数を使ってサイトのオリジンを取得する。
+ * @returns サイトのオリジン `http://localhost:4321`, `https://33.shikosai.net/` など
+ * @see https://docs.astro.build/ja/guides/environment-variables/
+ */
+const getSite = (): URL => {
+  // デプロイ先がCloudflare Pagesの場合
+  // @see https://developers.cloudflare.com/pages/configuration/build-configuration/
+  if (import.meta.env.CF_PAGES === '1' && import.meta.env.PAGES_URL) {
+    return new URL(import.meta.env.PAGES_URL);
+  }
+  // ローカル開発環境の場合
+  if (import.meta.env.DEV === true) {
+    return new URL('https://localhost:4321');
+  }
+  // わからない時
+  return new URL('https://33.shikosai.net');
+};
+
+console.info('🌐 The site origin was set to:', getSite().origin);
 
 export default defineConfig({
   prefetch: true,
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://shikosai.net/'
-      : process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}/`
-        : 'https://localhost:3000/',
+  site: getSite().origin,
   integrations: [
     react(),
     // NOTE: Sentry SDK is installed to use Spotlight in the local environment, so it is disabled in the production environment.
-    sentry({ enabled: !isProduction }),
+    sentry({ enabled: import.meta.env.DEV === true }),
     sitemap(),
     spotlightjs(),
     tailwind(),
