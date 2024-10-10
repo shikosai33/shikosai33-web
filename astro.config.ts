0f1d1dc9139dbe3d81d97058d522262e663fd8d5
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-10-10 12:08:52 +0900

    chore: 🔧 (`astro.config.ts`) Shell由来の環境変数を`process.env`から読み込むように修正した。  (#35)
    `import.meta.env`はViteにより上書きされて、Shell由来の環境変数にアクセスできない。
    そのため、Viteが提供する`.env*`ファイルからの読み込みとNODE_ENV相当の値は`import.meta.env`から、Shell由来の環境変数は`process.env`から読み込む
    - https://docs.astro.build/ja/guides/environment-variables/
    - https://ja.vite.dev/guide/env-and-mode
    - https://bun.sh/docs/api/import-meta

--- astro.config.ts
+++ astro.config.ts
@@ -6,14 +6,18 @@ import { defineConfig } from 'astro/config';
 
 /**
  * Astroの環境変数を使ってサイトのオリジンを取得する。
+ * `import.meta.env`はViteにより上書きされて、Shell由来の環境変数にアクセスできない。
+ * そのため、Viteが提供する`.env*`ファイルからの読み込みとNODE_ENV相当の値は`import.meta.env`から、Shell由来の環境変数は`process.env`から読み込む
  * @returns サイトのオリジン `http://localhost:4321`, `https://33.shikosai.net/` など
  * @see https://docs.astro.build/ja/guides/environment-variables/
+ * @see https://ja.vite.dev/guide/env-and-mode
+ * @see https://bun.sh/docs/api/import-meta
  */
 const getSite = (): URL => {
   // デプロイ先がCloudflare Pagesの場合
   // @see https://developers.cloudflare.com/pages/configuration/build-configuration/
-  if (typeof import.meta.env.CF_PAGES_URL === 'string') {
-    return new URL(import.meta.env.CF_PAGES_URL);
+  if (typeof process.env.CF_PAGES_URL === 'string') {
+    return new URL(process.env.CF_PAGES_URL);
   }
   // ローカル開発環境の場合
   if (import.meta.env.DEV) {
