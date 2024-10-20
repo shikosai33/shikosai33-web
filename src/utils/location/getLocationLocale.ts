c75bd214efbf9bd7508dc62ac4b6d3a948e66c42
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-10-20 23:13:52 +0900

    fix: 🐞 (`getLocationLocale`) JSDocの型指定を削除した。
    - Typescriptで指定するように勧めるエラーが出るため

--- src/utils/location/getLocationLocale.ts
+++ src/utils/location/getLocationLocale.ts
@@ -3,11 +3,11 @@ import type { InferEntrySchema } from 'astro:content';
 /**
  * 指定された建物名、階数、部屋名を元にロケーションの表示用テキストを取得します。
  *
- * @param {Object} params - ロケーション情報を含むオブジェクト。
- * @param {string} params.building-name - 建物名。
- * @param {number | undefined} params.floor - 階数。数値で指定されている場合のみ使用されます。
- * @param {string} params.room-name - 部屋名。
- * @returns {string} ロケーションのロケールを表す文字列。階数が指定されている場合は「建物名 階数階 部屋名」の形式、それ以外の場合は「建物名 部屋名」の形式で返されます。
+ * @param params - ロケーション情報を含むオブジェクト。
+ * @param params.building-name - 建物名。
+ * @param params.floor - 階数。数値で指定されている場合のみ使用されます。
+ * @param params.room-name - 部屋名。
+ * @returns ロケーションのロケールを表す文字列。階数が指定されている場合は「建物名 階数階 部屋名」の形式、それ以外の場合は「建物名 部屋名」の形式で返されます。
  */
 export const getLocationLocale = ({
   'building-name': buildingName,
